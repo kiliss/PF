d5b8359bc62304dae1b2f49bdce11794da2e58b0
Ander0399 <you@example.com>
2022-09-10 17:35:07 -0500

    metodo de ander

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+    if (Number.isInteger(numero)) {
+        if (numero % 2 == 0) {
+            return numero.toString(2)
+        } else {
+            return numero.toString(16)
+        }
     }
+    return "No es entero";
+}
