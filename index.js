c6685f60f47072c1e02903f94372d2bfe8a6ccc1
nesteruk2135 <nesteruk806@gmail.com>
2022-09-10 20:19:07 -0300

    JS terminado mal

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(number){
     // proximamente escribiremos codigo aqui
+    let num = number;
+    if (num%2===0){
+        let binary = (num % 2).toString();
+    for (; num > 1; ) {
+        num = parseInt(num / 2);
+        binary =  (num % 2) + (binary);
     }
+    }
+    else {
+        if (number < 0)
+  {
+    number = 0xFFFFFFFF + number + 1;
+  }
+
+  return number.toString(16).toUpperCase();
+}
+    }
+  
+   function gabriel(){
+    let misDatos=[{
+        nombre:'gabriel'},
+       { apellido:'nesteruk'},
+       { edad:'21'},
+       { nacionalidad:'argentino'}
+    ]
+    return  misDatos
+   } 
