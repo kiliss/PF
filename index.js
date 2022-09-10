245af5e7c44c6917ddd3bbae76a6fe3eea8c97e3
Sebastian <SebasTian.C.C1418@gmail.com>
2022-09-10 17:53:00 -0500

    Punto 9 Realizado

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function laCajaDePandora(numero){
     if (numero % 2 == 0) {
-        return 0 || 1;
+        return numero == 0 || 1;
     } else if (numero % 2 == 1) {
-        return 1 || 2 || 3|| 4 || 5 || 6 || 7 || 8 || 9;
+        return numero == 1 || 2 || 3|| 4 || 5 || 6 || 7 || 8 || 9;
     }
         
     }
+
+function sebastian(){
+    return {
+        "name": "sebastian",
+        "age": 18,
+        "nationality": "Colombian"
+    }
+}
