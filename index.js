ac23d2fc6af7e5cc741a78debb0a90c0433303b9
kiliss <fabigus14@gmail.com>
2022-09-10 19:48:48 -0300

    Codigo Robado

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui  Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+    if (numero % 2 == 0) {
+        return 0 || 1;
+    } else if (numero % 2 == 1) {
+        return 1 || 2 || 3|| 4 || 5 || 6 || 7 || 8 || 9;
+    }
+        // codigo robado de ander
     }
