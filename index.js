0563be5aad53d8c10ee0b58fa98c9a619a1c5f1c
Alejandro Javier Ledesma Miño <alejandrojavierlemi@gmail.com>
2022-09-10 19:44:46 -0300

    JS Finished

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
-    }
+    if(num % 2) return numero.toString(2);
+    else return numero.toString(16)
+}
