214d2e17a4956ed2a0c539da4a6291ee7e830bb2
Ander0399 <you@example.com>
2022-09-12 22:35:50 -0500

    Estructura componente Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,7 +5,38 @@ import style from "./style/Login.module.css";
 const Login = () => {
     return (
         <div className={style.container}>
-            Login
+            <form>
+                <h1>Login</h1>
+                <div className={"stilos"}>
+                    <label>Email: </label>
+                    <input
+                        name='email'
+                        type="email"
+                        placeholder='email@gmail.com'
+                    />
+                </div>
+
+                <div className={"stilos"}>
+                    <label>Password: </label>
+                    <input
+                        name='password'
+                        type='password' id="password"
+                        placeholder='******'
+                    />
+                </div>
+                <div className={"stilos"}>
+                    <button >Login</button>
+                </div>
+                <a href='#!'>
+                    Forgot Password
+                </a>
+
+            </form>
+
+            <label>Don't have an Account</label>
+            <Link to='/'>
+                <button>Register</button>
+            </Link>
         </div>
     )
 }
