d86157e2be1d8688cae3195e0e9116d3a6d62165
Agustín <agustinpincheira97@gmail.com>
2022-09-14 18:47:41 -0300

    actions para ser probadas

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// OBTENER MENUS
 export function getMenus(){
     return async function(dispatch){
         var json = await axios.get("http://localhost:3001/menus");
@@ -8,4 +9,64 @@ export function getMenus(){
             payload: json.data
         })
     };
+};
+
+// FILTRAR MENUS (*SE DEBE PROBAR*)
+export function getMenuFilters(name, filter){
+    return async function(dispatch){
+        try {
+            var json = await axios.get(`http://localhost:3001/menus?${name ? `name=${name}` : ''}${filter ? `&filter=${filter}` : ''}`);
+            return dispatch({
+                type: "GET_MENU_FILTER",
+                payload: json.data.length > 0 ? json.data[0] : {}
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// BORRAR MENUS (*SE DEBE PROBAR*)
+export function deleteMenu(name){
+    return async function(dispatch){
+        try {
+            var json = await axios.delete("http://localhost:3001/menus/"+name)
+            return dispatch({
+                type: "DELETE_MENU",
+                payload: json.data === "Menu deleted" ? name : ''
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+// OBTENER COMIDAS (*SE DEBE PROBAR*)
+export function getFoods(){
+    return async function(dispatch){
+        try {
+            var json = await axios.get("http://localhost:3001/foods");
+            return dispatch({
+                type: "GET_FOODS",
+                payload: json.data
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// FILTRAR COMIDAS (*SE DEBE PROBAR*)
+export function getFoodsFilters(name, order, filter){
+    return async function(dispatch){
+        try {
+            var json = await axios.get(`http://localhost:3001/foods?${name ? `name=${name}`: ''}${order ? `&order=${order}` : ''}${filter ? `&filter=${filter}` : ''}`)
+            return dispatch({
+                type: "GET_FOOD_FILTER",
+                payload: json.data.length > 0 ? json.data[0] : {}
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
