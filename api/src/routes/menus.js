e54ab7c002a657d6c285c7c40b26330c1416c3c5
Alejandro Javier Ledesma Miño <alejandrojavierlemi@gmail.com>
2022-09-15 00:36:19 -0300

    Menu Component V2

--- api/src/routes/menus.js
+++ api/src/routes/menus.js
@@ -9,7 +9,7 @@ const getDBInfoo = async () => {
     return await Menu.findAll({
         include: {
             model: Food,
-            attributes: ['name', "photo", "summary", "price", "stock","drinkable"],
+            attributes: ['name', "photo", "summary", "price", "stock", "drinkable"],
             through: {
                 attributes: [],
             },
@@ -18,120 +18,77 @@ const getDBInfoo = async () => {
 };
 
 router.get('/', async (req, res) => {
-    if(req.query.name && req.query.filter){
-        try{
-            let filterfoodmenu = await Menu.findAll({
-                where:{
-                    name:{
-                        [Op.iLike]: `%${req.query.name}%`
-                    }
-                },
-                include: {
-                    model: Food,
-                    where: {
-                        drinkable: req.query.filter,
-                    },
-                    
-                    attributes: ['name', "photo", "summary", "price", "stock","drinkable"], 
-                    through: {
-                        attributes: [],
-                    },
-                }
-            })
-            res.status(201).send(filterfoodmenu);
-
-        }
-        catch(error){
-            return res.status(400).json("error "+error.message)
-        }
-
-    } else if(req.query.name){
-        try{
-            let findName=await Menu.findAll({
-                where:{
-                    name:{
-                        [Op.iLike]: `%${req.query.name}%`
-                    }
-                },
-                include:Food
-            })
-            res.status(201).send(findName);
-            
-    }catch(error){
-        return res.status(400).json("error "+error.message)
-    }
-
-    } else if(req.query.filter){
-        try{
-            let filterfoodmenu = await Menu.findAll({
-                include: {
-                    model: Food,
-                    where: {
-                        drinkable: req.query.filter,
-                    },
-                    attributes: ['name', "photo", "summary", "price", "stock","drinkable"],
-                    through: {
-                        attributes: [],
-                    },
-                }
-            })
-            res.status(201).send(filterfoodmenu);
-
-        }
-        catch(error){
-            return res.status(400).json("error "+error.message)
-        }
-
-    } else{
-        try{
-            let allMenus=await Menu.findAll({
-                include:Food
-            })
-            res.status(201).send(allMenus);
-        }catch(error){
-            return res.status(400).json("error "+error.message)
-        }
-    }
-});
-
-router.get('/:name', async (req, res) => {
-    const { name } = req.params;
-    if(name){
-        try{
-            let findName=await Menu.findAll({
-                where:{
-                    name:{
+    const { name, filter = "both", price = 'id' } = req.query;
+    try {
+        if (name) {
+            let menu = await Menu.findOne({
+                where: {
+                    name: {
                         [Op.iLike]: `%${name}%`
                     }
                 },
-                include:Food
+                include: [{
+                    model: Food,
+                    where: {
+                        drinkable: {
+                            [Op.or]: filter === "drink" ? [true] : filter === "food" ? [false] : [true, false]
+                        }
+                    }
+                }],
+                order: [
+                    price.toUpperCase() === "ASC" || price.toUpperCase() === "DESC" ? [Food, 'price', price.toUpperCase()] : [Food, 'id', 'DESC']
+                ]
+            });
+            if(!menu) throw "menu not found";
+            return res.status(201).send(menu);
+        } else {
+            return res.status(201).send(await Menu.findAll());
+        }
+    }
+    catch (err) {
+        return res.status(400).json(err)
+    }
+})
+
+router.get('/:name', async (req, res) => {
+    const { name } = req.params;
+    if (name) {
+        try {
+            let findName = await Menu.findAll({
+                where: {
+                    name: {
+                        [Op.iLike]: `%${name}%`
+                    }
+                },
+                include: Food
             })
             res.status(201).send(findName);
-    }catch(error){
-        return res.status(400).json("error "+error.message)
+        } catch (error) {
+            return res.status(400).json("error " + error.message)
+        }
+
     }
-    
-}})
+})
 
 
 router.post('/', async (req, res) => {   // Crea menu
     const { name, photo, description } = req.body;
-    try{
-    Menu.create({
-        name: name,
-        photo: photo,
-        description: description,
-    });
-    info = await getDBInfoo();
-    res.status(201).send("Menu created");
+    try {
+        Menu.create({
+            name: name,
+            photo: photo,
+            description: description,
+        });
+        info = await getDBInfoo();
+        res.status(201).send("Menu created");
     } catch (error) {
-        return res.status(400).json("error "+error.message)
+        return res.status(400).json("error " + error.message)
     }
 
 });
 
 router.delete('/:name', async (req, res) => {
-    try{
+    try {
         let menu = await Menu.findOne({
             where: {
                 name: req.params.name
@@ -144,8 +101,8 @@ router.delete('/:name', async (req, res) => {
             res.status(404).send("Menu not found");
         }
     }
-    catch(error){
-        return res.status(400).json("error "+error.message)
+    catch (error) {
+        return res.status(400).json("error " + error.message)
     }
 })
 
@@ -155,15 +112,15 @@ router.put('/:name', async (req, res) => {
             where: {
                 name: req.params.name
             }
-            });
-            if (menu) {
-                await menu.update();
-                res.status(200).send("Menu updated");
-            } else {
-                res.status(404).send("Menu not found");
-            }
+        });
+        if (menu) {
+            await menu.update();
+            res.status(200).send("Menu updated");
+        } else {
+            res.status(404).send("Menu not found");
+        }
     } catch (error) {
-        return res.status(400).json("error "+error.message)
+        return res.status(400).json("error " + error.message)
     }
 })
 
