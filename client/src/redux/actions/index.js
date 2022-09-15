470af8456ec51ca014968e28a75b2bef0d142653
Agustín <agustinpincheira97@gmail.com>
2022-09-15 15:20:27 -0300

    actions terminadas para menus, foods, users, resevations

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -11,7 +11,7 @@ export function getMenus(){
     };
 };
 
-// FILTRAR MENUS (*SE DEBE PROBAR*)
+// FILTRAR MENUS
 export function getMenu(data){
     return async function(dispatch){
         try {
@@ -26,6 +26,18 @@ export function getMenu(data){
     };
 };
 
+// CREAR MENU (*SE DEBE PROBAR*)
+export function createMenu(payload){
+    return async function(dispatch){
+        try {
+            var json = await axios.post("http://localhost:3001/menus", payload)
+            return json;
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
 // BORRAR MENUS (*SE DEBE PROBAR*)
 export function deleteMenu(name){
     return async function(dispatch){
@@ -34,6 +46,21 @@ export function deleteMenu(name){
             return dispatch({
                 type: "DELETE_MENU",
                 payload: json.data === "Menu deleted" ? name : ''
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// UPDATE MENU (*SE DEBE PROBAR*)
+export function updateMenu(name){
+    return async function(dispatch){
+        try {
+            var json = await axios.put("http://localhost:3001/menus/"+name)
+            return dispatch({
+                type: "UPDATE_MENU",
+                payload: json.data
             })
         } catch (error) {
             console.log(error)
@@ -56,6 +83,21 @@ export function getFoods(){
     };
 };
 
+// OBTENER DETALLES DE COMIDAS POR ID (*SE DEBE PROBAR)
+export function getFoodDetail(id){
+    return async function(dispatch){
+        try {
+            var json = await axios.get("http://localhost:3001/foods/"+id)
+                return dispatch({
+                    type: "GET_FOOD_DETAIL",
+                    payload: json.data
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
 // FILTRAR COMIDAS (*SE DEBE PROBAR*)
 export function getFoodsFilters(name, order, filter){
     return async function(dispatch){
@@ -68,5 +110,108 @@ export function getFoodsFilters(name, order, filter){
         } catch (error) {
             console.log(error)
         }
+    };
+};
+
+// AGREGAR COMIDA A MENU EXISTENTE
+export function addFoodToMenu(payload){
+    return async function(dispatch){
+        try {
+            var json = await axios.post("http://localhost:3001/foods/tomenu"+payload)
+            return json
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
+
+// BORRAR COMIDA (*SE DEBE PROBAR*)
+export function deleteFood(id){
+    return async function(dispatch){
+        try {
+            var json = await axios.delete("http://localhost:3001/foods/"+id)
+            return dispatch({
+                type: "DELETE_FOOD",
+                payload: json.data === "food borrada" ? id : ''
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// OBTENER USERS (*SE DEBE PROBAR*)
+export function getUsers(){
+    return async function(dispatch){
+        var json = await axios.get("http://localhost:3001/users/users");
+        return dispatch({
+            type: "GET_USERS",
+            payload: json.data
+        })
+    };
+};
+
+// OBTENER DETALLE DE USER POR ID (*SE DEBE PROBAR*)
+export function getUserDetail(id){
+    return async function(dispatch){
+        try {
+            var json = await axios.get("http://localhost:3001/users/"+id)
+                return dispatch({
+                    type: "GET_USER",
+                    payload: json.data
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// CREAR UN USUARIO (*SE DEBE PROBAR*)
+export function createUser(payload){
+    return async function(dispatch){
+        try {
+            var json = await axios.post("http://localhost:3001/users", payload)
+            return json;
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// OBTENER RESERVAS (*SE DEBE PROBAR*)
+export function getReservations(){
+    return async function(dispatch){
+        var json = await axios.get("http://localhost:3001/reservation/users");
+        return dispatch({
+            type: "GET_RESERVATIONS",
+            payload: json.data
+        })
+    };
+};
+
+// OBTENER DETALLE DE UNA RESERVA POR ID (*SE DEBE PROBAR*)
+export function getReservationDetail(id){
+    return async function(dispatch){
+        try {
+            var json = await axios.get("http://localhost:3001/reservation/"+id)
+                return dispatch({
+                    type: "GET_RESERVATION",
+                    payload: json.data
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
+
+// CREAR UNA RESERV (*SE DEBE PROBAR*)
+export function createReservation(payload){
+    return async function(dispatch){
+        try {
+            var json = await axios.post("http://localhost:3001/reservation", payload)
+            return json;
+        } catch (error) {
+            console.log(error)
+        }
+    };
+};
