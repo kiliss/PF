a97760cfcc0e87524ad0fa5740e91c0076a665e7
Alejandro Javier Ledesma Miño <alejandrojavierlemi@gmail.com>
2022-09-16 12:55:07 -0300

    Dark Mode in Navbar was Deleted, Initial ID in Menu, Food and Menu_Food works

--- api/index.js
+++ api/index.js
@@ -7,10 +7,13 @@ async function loadMenus() {
   Menu.bulkCreate(consumir.menu);
   Food.bulkCreate(consumir.food);
   Menu_food.bulkCreate(consumir.menu_food);
+  await conn.query("SELECT setval('public.menus_id_seq', 1000, true)");
+  await conn.query("SELECT setval('public.food_id_seq', 1000, true)");
+  await conn.query("SELECT setval('public.menu_foods_id_seq', 1000, true)");
 };
 
 
-conn.sync({ force: true }).then(async() => {
+conn.sync({ force: true }).then(async () => {
   loadMenus();
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001");
