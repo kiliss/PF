4ad0e1b3dcdc5cbcbcd756169327f6adea1a5035
Sebastian <SebasTian.C.C1418@gmail.com>
2022-09-21 22:44:09 -0500

    Login with google failed

--- api/src/app.js
+++ api/src/app.js
@@ -1,76 +1,67 @@
-const express = require('express');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const foods = require('./routes/foods');
-const menus = require('./routes/menus');
-require('./auth/passport');
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const foods = require("./routes/foods");
+const menus = require("./routes/menus");
+const passport = require("passport");
+require("./auth/passport")(passport);
+const session = require("express-session");
 
-// const cookieSession = require('cookie-session');
-// const passport = require('passport');
-const login = require('./routes/login');
-const users = require('./routes/users');
-const reservation = require('./routes/reservation');
-const authRoute = require('./routes/auth');
-const feedbacks = require('./routes/feedbacks');
-const table=require('./routes/table')
+const cookieSession = require('cookie-session');
 
+const login = require("./routes/login");
+const users = require("./routes/users");
+const reservation = require("./routes/reservation");
+const auth = require("./routes/auth");
+const feedbacks = require("./routes/feedbacks");
+const table = require("./routes/table");
 
+const cors = require("cors");
 const server = express();
-const cors = require('cors');
-
-
-// server.use(cookieSession({
-//     name: "session",
-//     keys:["pgrestaurant"],
-//     maxAge: 24*60*60*1000
-// }));
-// server.use(passport.initialize());
-// server.use(passport.session({}));
 
 // server.use(cors({
-//     origin:'*',
-//     methods: 'GET,POST,PUT,DELETE',
-//     credentials: true,
+
 // }));
-server.use(cors())
 
-// server.use(
-//     cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
-//   );
-  
-//   server.use(passport.initialize());
-//   server.use(passport.session());
-  
-//   server.use(
-//     cors({
-//       origin: "*",
-//       methods: "GET,POST,PUT,DELETE",
-//       credentials: true,
-//     })
-//   );
-  
-//   server.use("/auth", authRoute);
+server.use(cookieSession({
+    name: "user",
+    keys:["pgrestaurant"],
+    maxAge: 24*60*60*1000
+}));
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cors({
+    origin:'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+}));
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-server.use(morgan('dev'));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+
+server.use(morgan("dev"));
 server.use(express.json());
 
+server.use(
+  session({
+    secret: "secret",
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
 
-
-server.use('/table',table);
-server.use('/foods', foods);
-server.use('/menus', menus);
-// server.use('/auth',auth);
-server.use('/users', users);
-server.use('/reservation', reservation);
-server.use('/login', login);
-server.use('/feedbacks',feedbacks);
+server.use("/table", table);
+server.use("/foods", foods);
+server.use("/menus", menus);
+server.use("/auth", auth);
+server.use("/users", users);
+server.use("/reservation", reservation);
+server.use("/login", login);
+server.use("/feedbacks", feedbacks);
 
 module.exports = server;
-
-
-
-
--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,19 +1,95 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../db.js");
 
-const CLIENT_URL = "";
- 
+const CLIENT_URL = "http://localhost:3000/";
 
-router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
+function passwordRandom() {
+  return "secret";
+}
+// router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
+
+// router.get(
+//   "/github/callback",
+//   passport.authenticate("github", {
+//     successRedirect: CLIENT_URL,
+//     failureRedirect: "/login/failed",
+//   })
+// );
+
+router.get("/login/success", (req, user) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "succesfull",
+      user: req.user,
+    });
+  }
+});
+
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/login");
+});
+
+// router.get('/facebook', passport.authenticate('facebook', { scope: ['profile'] }));
+
+// router.get('/facebook/callback', passport.authenticate('facebook',
+
+// { successRedirect: CLIENT_URL, failureRedirect: '/login'}
+
+// ))
 
 router.get(
-  "/github/callback",
-  passport.authenticate("github", {
-    successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed",
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
   })
 );
 
-module.exports = router
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  async (req, res) => {
+    // Successful authentication, redirect home.
+    console.log(req.user);
+    const { googleId: id, displayName, emails, photos } = req.user;
+    try {
+      var userEmail = await User.findOne({
+        where: { googleId: googleId },
+      }).catch((err) => {
+        console.log("Error: ", err);
+      });
+      // console.log('bd user :',userEmail)
+      if (!userEmail) {
+        userEmail = await User.create({
+          googleId: googleId,
+          user: displayName,
+          email: emails[0].value,
+          photo: photos[0].value,
+          password: bcrypt.hash(passwordRandom(), 10),
+        });
+      }
+      const jwtToken = jwt.sign(
+        JSON.stringify({
+          id: userEmail.id,
+          email: userEmail.email,
+          user: userEmail.user,
+          photo: userEmail.photo,
+        }),
+        process.env.JWT_SECRET
+      );
+      res.redirect("/").json(jwtToken);
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  }
+);
 
+module.exports = router;
